646f22733731d97d69dba322f870e03ba6558156
Stephen Buttolph <stephen@avalabs.org>
2024-12-18 17:39:19 -0500

    nits

--- interface.go
+++ interface.go
@@ -8,9 +8,6 @@ type Message interface {
 	// It is assumed that this message is ValidCanoto.
 	MarshalCanoto() []byte
 	// UnmarshalCanoto unmarshals a Canoto-encoded byte slice into the message.
-	//
-	// The message is not cleared before unmarshaling, any fields not present in
-	// the bytes will retain their previous values.
 	UnmarshalCanoto(bytes []byte) error
 }
 
@@ -23,20 +20,17 @@ type Field interface {
 	//
 	// It is assumed that this field is ValidCanoto.
 	MarshalCanotoInto(w *Writer)
-	// CalculateCanotoCache populates size and oneOf caches based on the current
+	// CalculateCanotoCache populates internal caches based on the current
 	// values in the struct.
 	CalculateCanotoCache()
 	// CachedCanotoSize returns the previously calculated size of the Canoto
 	// representation from CalculateCanotoCache.
 	//
 	// If CalculateCanotoCache has not yet been called, or the field has been
-	// modified since the last call to CalculateCanotoCache, or the returned
-	// size may be incorrect.
+	// modified since the last call to CalculateCanotoCache, the returned size
+	// may be incorrect.
 	CachedCanotoSize() int
 	// UnmarshalCanotoFrom populates the field from a canoto.Reader.
-	//
-	// The field is not cleared before unmarshaling, any sub-fields not present
-	// in the bytes will retain their previous values.
 	UnmarshalCanotoFrom(r *Reader) error
 	// ValidCanoto validates that the field can be correctly marshaled into the
 	// Canoto format.
