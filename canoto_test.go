4c156395eb900c173184ca6338578b8a3baee29f
Stephen Buttolph <stephen@avalabs.org>
2025-03-09 11:23:37 -0400

    Fix library generation

--- canoto_test.go
+++ canoto_test.go
@@ -986,3 +986,13 @@ func TestSizeOf(t *testing.T) {
 	require.Equal(SizeEnum64, sizeOf[int64]())
 	require.Equal(SizeEnum64, sizeOf[uint64]())
 }
+
+func TestIsBytesEmpty(t *testing.T) {
+	require := require.New(t)
+
+	require.True(isBytesEmpty(make([]byte, 0)))
+	require.True(isBytesEmpty(make([]byte, 10)))
+
+	require.False(isBytesEmpty([]byte{0: 1}))
+	require.False(isBytesEmpty([]byte{10: 1}))
+}
