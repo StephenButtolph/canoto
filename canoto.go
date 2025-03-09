d76721018e3de2caca284fa9386ac49bb42cffc9
Stephen Buttolph <stephen@avalabs.org>
2025-03-09 12:18:48 -0400

    Update version

--- canoto.go
+++ canoto.go
@@ -47,7 +47,7 @@ const (
 	MaxFieldNumber = 1<<29 - 1
 
 	// Version is the current version of the canoto library.
-	Version = "v0.11.1"
+	Version = "v0.12.0"
 
 	wireTypeLength = 3
 	wireTypeMask   = 0x07
