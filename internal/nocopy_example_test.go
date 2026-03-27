1983dad305f04f1fc6c332310ea452236a39ef13
Stephen Buttolph <stephen@avalabs.org>
2026-03-27 17:08:04 -0400

    Simplify Example_copyRace (#157)

--- internal/nocopy_example_test.go
+++ internal/nocopy_example_test.go
@@ -9,18 +9,9 @@ package examples
 // Example_copyRace demonstrates the potential benefit of the "nocopy" tag.
 // Without it, users may encounter data-races.
 func Example_copyRace() {
-	var (
-		s    OneOf // doesn't use nocopy
-		done = make(chan struct{})
-	)
-	go func() {
-		defer close(done)
-		s.CalculateCanotoCache()
-	}()
-
-	// Read the atomic values concurrently with the writes.
+	var s OneOf
+	go s.CalculateCanotoCache()
 	_ = s
 
-	<-done
 	// Output:
 }
