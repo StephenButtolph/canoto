248d78dc88500bef92a2045394e2135764004df0
Stephen Buttolph <stephen@avalabs.org>
2026-03-28 15:06:05 -0400

    nit

--- generate/doc.go
+++ generate/doc.go
@@ -1,4 +1,4 @@
 // Package generate provides code generation for canoto serialization. It parses
-// Go source files for canoto struct tags and generates marshaling and
+// Go source files for canoto struct tags. It generates marshaling and
 // unmarshaling code, proto file definitions, and standalone library copies.
 package generate
